307def26e6b61f6f1513a9196e926cb620fceeac
omsfuk <omsfuk@163.com>
2024-05-22 19:44:08 +0800

    Update update_blockwise.sh

--- update_blockwise.sh
+++ update_blockwise.sh
@@ -4,6 +4,13 @@ if [ -z "$1" ]; then
     exit 1
 fi
 
-rm -rf /opt/tiger/flow/flowservice/blockwise/blockwise
-git clone --branch "$1" https://code.byted.org/flowlang/blockwise.git /opt/tiger/flow/flowservice/blockwise/blockwise
+# if [ ! -d /opt/tiger/flow/flowservice/dist-info ]; then
+#  mkdir -p /opt/tiger/flow/flowservice/dist-info
+#  cp -r /opt/tiger/flow/flowservice/flow_engine/blockwise/bytedblockwise-* /opt/tiger/flow/flowservice/dist-info/
+# fi
+
+rm -rf /opt/tiger/flow/flowservice/flow_engine/blockwise
+git clone --branch "$1" https://code.byted.org/flowlang/blockwise.git /opt/tiger/flow/flowservice/flow_engine/blockwise
+# cp -r /opt/tiger/flow/flowservice/dist-info/ /opt/tiger/flow/flowservice/flow_engine/blockwise/
+
 kill $(pgrep -f "bizengine")
